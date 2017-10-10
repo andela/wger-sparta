c9f33079d883a8fc777c6a5a052a5c4808234ae3
ridgekimani <ridgekimani@gmail.com>
2017-10-10 16:53:57 +0300

    ft(tests for activate and deactivate trainer): - Tests for activating and deactivating the user
     [Delivers 151593402]

---
+++ wger/core/tests/test_deactivate_user.py
@@ -0,0 +1,31 @@
+import logging
+
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+from wger.core.tests.base_testcase import WorkoutManagerTestCase
+
+logger = logging.getLogger(__name__)
+
+
+class DeactivateUserTestCase(WorkoutManagerTestCase):
+    """
+    Test deactivate and activate user account
+    """
+
+    def deactivate_user(self, fail=False):
+        """
+        Helper function to test activate and deactivate the user
+        :param fail:
+        :return:
+        """
+        self.user_login()
+        user = User.objects.get(username=self.current_user)
+        response = self.client.get(reverse('core:user:deactivate', kwargs=dict(pk=user.id)))
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(user.is_active) if fail else self.assertTrue(user.is_active)
+
+        # Test activate user
+        response = self.client.get(reverse('core:user:activate', kwargs=dict(pk=user.id)))
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(user.is_active) if fail else self.assertTrue(user.is_active)
