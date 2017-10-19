1a1d18cabc65f45a5b2c245d53fffbe728a67a20
Descartess <nyondopaul23@gmail.com>
2017-10-19 12:57:45 +0300

    Add fitbit support

--- wger/urls.py
+++ wger/urls.py
@@ -150,8 +150,6 @@ urlpatterns = i18n_patterns(
         {'sitemaps': sitemaps},
         name='sitemap'),
     url(r'^social', include('social_django.urls', namespace="social")),
-    url(r'^fitbit/', include('fitapp.urls')),
-
 )
 #
 # URLs without language prefix
