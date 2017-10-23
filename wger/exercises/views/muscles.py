d86c50d3fb460ab18ced35c5cca076f93500d817
ndero <john.waithaka@andela.com>
2017-10-23 16:04:33 +0300

    bg: get_queryset method to autorefresh the page after muscle deletion

--- wger/exercises/views/muscles.py
+++ wger/exercises/views/muscles.py
@@ -43,9 +43,13 @@ class MuscleListView(ListView):
     Overview of all muscles and their exercises
     '''
     model = Muscle
-    queryset = Muscle.objects.all().order_by('-is_front', 'name'),
     context_object_name = 'muscle_list'
     template_name = 'muscles/overview.html'
+
+    def get_queryset(self):
+        if self.template_name == 'muscles/overview.html':
+            return Muscle.objects.all().order_by('-is_front', 'name'),
+        return Muscle.objects.all().order_by('-is_front', 'name')
 
     def get_context_data(self, **kwargs):
         '''
